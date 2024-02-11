3af0c24e897ffb857e6eadb2a3503f686684a32c
Anish Balami <anishbalami38@gmail.com>
2024-02-11 17:43:32 +0545

    Final touch

---
+++ employee-system-api/src/main/java/com/anishpro/Employee/services/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.anishpro.Employee.services;
+
+import com.anishpro.Employee.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService
+{
+    Employee createEmployee(Employee employee);
+
+    List<Employee> getAllEmployees();
+
+    boolean deleteEmployee(Long id);
+
+    Employee getEmployeeById(Long id);
+
+    Employee updateEmployee(Long id, Employee employee);
+}
